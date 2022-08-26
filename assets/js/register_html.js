5b8c4fd16d9765a8ed37f15989590dcc7f6ec969
ebalin1411 <ebalinrosecse2006@gmail.com>
2022-08-26 09:53:08 +0530

    after js

---
+++ assets/js/register_html.js
@@ -0,0 +1,13 @@
+
+function onKeyPress(event){
+    console.log("you pressed",event.key);
+    const username = document.querySelector('.input name');
+    console.log(username);
+
+    
+    
+}
+
+
+
+
